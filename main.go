f70106934a072c74c5e58a744a7c3a614b1bb4fe
98WuG <gerald@geraldwu.com>
2021-04-02 00:53:10 -0400

    handle errors more gracefully

--- main.go
+++ main.go
@@ -30,7 +30,9 @@ func HTTPServer(w http.ResponseWriter, r *http.Request) {
     // Determine if this is a direct i.imgur.com image
     directImage, err := regexp.MatchString(".*\\.(jpg|jpeg|png|gif|gifv|apng|tiff|mp4|mpeg|avi|webm)", uri)
     if err != nil {
-        log.Fatal(err)
+        log.Print(err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
     }
 
     if directImage {
@@ -39,6 +41,7 @@ func HTTPServer(w http.ResponseWriter, r *http.Request) {
     } else {
         // Future proxying features not yet implemented
         http.Error(w, "501 Not Implemented", http.StatusNotImplemented)
+        return
     }
 }
 
@@ -46,7 +49,9 @@ func DirectImageHandler(w http.ResponseWriter, uri string) {
     // Get the image directly from i.imgur.com
     resp, err := http.Get(fmt.Sprintf("https://i.imgur.com/%v", uri))
     if err != nil {
-        log.Fatal(err)
+        log.Print(err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
     }
     defer resp.Body.Close()
 
@@ -63,6 +68,8 @@ func DirectImageHandler(w http.ResponseWriter, uri string) {
     w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
 
     if _, err = io.Copy(w, resp.Body); err != nil {
-        log.Fatal(err)
+        log.Print(err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
     }
 }
