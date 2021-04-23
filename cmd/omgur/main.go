a9df5d3b7d903d1fcdc3207b7e088b0dd8ab3f77
98WuG <gerald@geraldwu.com>
2021-04-23 14:30:51 -0400

    create static endpoint separate out css from album html template

--- cmd/omgur/main.go
+++ cmd/omgur/main.go
@@ -11,6 +11,7 @@ func main() {
 	app, _ := app.CreateApp("Client-ID 546c25a59c58ad7")
 
 	http.HandleFunc("/", app.HTTPServer)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/template/static"))))
 
 	log.Print("Starting webserver on 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
