4aa46df16147ff6e026364d9cc43b7b746efc711
DUO Labs <dvdugo333@gmail.com>
2021-11-28 16:54:23 -0500

    Added ability to change port from environment variable

--- cmd/omgur/main.go
+++ cmd/omgur/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
-
+	"os"
 	"git.geraldwu.com/gerald/omgur/pkg/app"
 )
 
@@ -13,8 +13,17 @@ func main() {
 	http.HandleFunc("/", app.HTTPServer)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/template/static"))))
 
-	log.Print("Starting webserver on 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	getenv:=func (key, fallback string) string {
+	    value := os.Getenv(key)
+	    if len(value) == 0 {
+	        return fallback
+	    }
+	    return value
+	}
+
+	port:=getenv("PORT","8080")
+	log.Print("Starting webserver on "+port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
