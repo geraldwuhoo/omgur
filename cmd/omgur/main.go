158cfaa3ec1ae8f0710a82d727aa3b84062abd9e
DUO Labs <dvdugo333@gmail.com>
2021-11-28 23:00:37 -0500

    Embed the css file into binary

--- cmd/omgur/main.go
+++ cmd/omgur/main.go
@@ -5,13 +5,17 @@ import (
 	"net/http"
 	"os"
 	"git.geraldwu.com/gerald/omgur/pkg/app"
+	"embed"
 )
+
+//go:embed *.css
+var content embed.FS
 
 func main() {
 	app, _ := app.CreateApp("Client-ID 546c25a59c58ad7")
 
 	http.HandleFunc("/", app.HTTPServer)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/template/static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(content))))
 
 	getenv:=func (key, fallback string) string {
 	    value := os.Getenv(key)
